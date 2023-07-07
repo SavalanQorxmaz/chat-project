a589732c5ecf42bf05b385754e91937006542abc
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-07-07 13:22:20 +0400

    error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,31 @@
-
+import { useEffect } from "react";
+import ChatPage from "./pages/ChatPage";
 
 function App() {
 
+  const getData = async ()=> {
+    const data = await fetch('http://localhost:9000',{
+      method: 'Get',
+      
+      headers:{
+        "Content-Type": "application/json",
+      }
+    })
+    .then(res=>res.json())
+    return data
+  }
 
-  fetch('http://localhost:9000',{
-    method: 'Get',
-    
-    headers:{
-      "Content-Type": "application/json",
-    }
-  })
-  .then(res=>res.json())
+  useEffect(()=>{
+    getData()
   .then(res=>console.log(res))
+  },[])
+  
 
 
   return (
     <div className="App">
       
-
+    <ChatPage/>
       
     </div>
   );
---
+++ frontend/src/pages/ChatPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SelectedChat from '../components/SelectedChat'
+
+const ChatPage = () => {
+  return (
+    <div>
+        <SelectedChat/>
+    </div>
+  )
+}
+
+export default ChatPage
