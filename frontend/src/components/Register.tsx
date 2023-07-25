b3d3b54679637cf6c9a4207446bcb0521f6d893f
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-07-25 17:37:12 +0400

    register component almost ready

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,33 +1,144 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faEye} from '@fortawesome/free-solid-svg-icons'
+import {faEyeSlash} from '@fortawesome/free-solid-svg-icons'
+
 
 const Register = ({...props}) => {
 
   const {loginOrRegister, setLoginOrRegister} = props
+  
+  const [dataIsReady, setDataIsReady] = useState({
+    userNameIsReady: false,
+    passwordIsReady: false,
+    confirmPasswordIsReady: false,
+    allDataIsReady:false
+  })
+  const [registerData, setRegisterData] = useState({
+    userName: '',
+    password: '',
+    confirmPassword: ''
+  })
 
+  const [showPassword, setShowPassword] = useState({
+    showPassword: false,
+    showConfirmPassword: false
+  })
+
+  const userNameRef = useRef()
+  const passwordRef = useRef()
+  const confirmPasswordRef = useRef()
+
+
+  const currentRegisterDataF = (e:any)=>{
+     setRegisterData((prev=>({...prev,[e.target.name]:e.target.value} )))
+  } 
+
+  const resetRegisterDataF = ()=>{
+    setRegisterData({
+      userName: '',
+      password: '',
+      confirmPassword: ''   
+  })
+
+  setShowPassword({
+    showPassword:false,
+    showConfirmPassword:false
+  })
+}
+
+useEffect(()=>{
+// UserName check
+  registerData.userName.length >4 
+  ? 
+  setDataIsReady(prev=>({...prev, userNameIsReady:true}))
+  :
+  setDataIsReady(prev=>({...prev, userNameIsReady:false}))
+
+// Password check
+
+  registerData.password.search(/[A-Z]/)<0 || registerData.password.search(/[a-z]/) < 0 || registerData.password.search(/[0-9]/) < 0 || registerData.password.search(/[@,!,#,$,%,&,*]/) < 0 || registerData.password.length < 8 
+  ?
+  setDataIsReady(prev=>({...prev, passwordIsReady:false}))
+  :
+
+  setDataIsReady(prev=>({...prev, passwordIsReady:true}))
+  
+// Confirm Password check
+  registerData.password !== registerData.confirmPassword || registerData.confirmPassword==='' 
+  ?
+  setDataIsReady(prev=>({...prev, confirmPasswordIsReady:false}))
+  : 
+  setDataIsReady(prev=>({...prev, confirmPasswordIsReady:true}))
+
+
+
+
+},[registerData])
+
+  // All data is ready?
+useEffect(()=>{
+  
+
+    dataIsReady.userNameIsReady && dataIsReady.passwordIsReady && dataIsReady.confirmPasswordIsReady
+    ?
+    setDataIsReady(prev=>({...prev, allDataIsReady: true}))
+    :
+    setDataIsReady(prev=>({...prev, allDataIsReady: false}))
+},[dataIsReady])
+
+const registerF = ()=>{
+
+}
 
   return (
     <form className='w-full flex flex-col items-center justify-center  select-none '>
 
      
-    <label htmlFor="user-name" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
-      <span className='w-1/3 text-right mr-3'>User Name:</span>
-    <input className='w-2/3 outline-none p-1 bg-inherit' id='user-name' type="text" placeholder='Filankes' />
+    <label htmlFor="user-name" className='w-full p-3 py-1 m-3 mb-1 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
+      <span className='w-2/5 text-right mr-3'>User Name:</span>
+    <input ref={userNameRef.current} onChange={currentRegisterDataF}  className='w-2/3 outline-none p-1 bg-inherit' id='user-name' type="text" name='userName' placeholder='Filankes' />
     </label>
+
+    <p className={!dataIsReady.userNameIsReady ? 'w-full p-3 py-1 mb-3 rounded-lg   text-red-400 text-center  ': 'hidden'}>Minimum 5 simvol, Herfle baslamalidi</p>
    
    <label htmlFor="password" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
-    <span className='w-1/3 text-right mr-3'>Password:</span>
-   <input className='w-2/3 outline-none p-1 bg-inherit' id='password' type="password" placeholder='********' />
-   </label>
-   <label htmlFor="password" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
-    <span className='w-1/3 text-right mr-3'>Password Confirm:</span>
-   <input className='w-2/3 outline-none p-1 bg-inherit' id='password' type="password" placeholder='********' />
-   </label>
+    <span className='w-2/5 text-right mr-3'>Password:</span>
+   <input onChange={currentRegisterDataF} className='w-2/3 outline-none p-1 bg-inherit' id='password' type={showPassword.showPassword? 'text':'password'} name='password' placeholder='********' />
+   {
+    showPassword.showPassword
+    ?
     
-   <input type="submit" value="SignUp" className='w-full px-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none cursor-pointer'/>
+   <FontAwesomeIcon onClick={()=>setShowPassword(prev=>({...prev,showPassword:false}))} icon={faEyeSlash} />
+   :
+   <FontAwesomeIcon  onClick={()=>setShowPassword(prev=>({...prev,showPassword:true}))} icon={faEye} />
+
+   }
+   </label>
+
+   <p className={!dataIsReady.passwordIsReady ? 'w-full p-3 py-1 mb-3  text-center  text-red-400   ': 'hidden'}>Minimum 8 simvol, Boyukherf, kicik herf, reqem, !@#$%&*</p>
+
+   <label htmlFor="confirm-password" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
+    <span className='w-2/5 text-right mr-3'>Password Confirm:</span>
+   <input onChange={currentRegisterDataF}  className='w-2/3 outline-none p-1 bg-inherit' id='confirm-password' type={showPassword.showConfirmPassword? 'text': "password"} name='confirmPassword' placeholder='********' />
+   {
+    showPassword.showConfirmPassword
+    ?
+    
+    <FontAwesomeIcon onClick={()=>setShowPassword(prev=>({...prev,showConfirmPassword:false}))} icon={faEyeSlash} />
+    :
+    <FontAwesomeIcon  onClick={()=>setShowPassword(prev=>({...prev,showConfirmPassword:true}))} icon={faEye} />
+
+   }
+   </label>
+
+   <p className={!dataIsReady.confirmPasswordIsReady ? 'w-full p-3 py-1 mb-3  text-center  text-red-400   ': 'hidden'}>Parollar eyni olmalidi</p>
+    
+   <input type="submit" value="SignUp" className={dataIsReady.allDataIsReady? 'w-full px-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none cursor-pointer': 'w-full px-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-not-allowed'} disabled = {!dataIsReady.userNameIsReady || !dataIsReady.passwordIsReady || !dataIsReady.confirmPasswordIsReady ? true:false }/>
 
    <div className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 '>
 
-    <input type="reset" value="Clean" className='w-1/2 text-center m-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer hover:scale-95 hover:shadow-none '/>
+    <input onClick={resetRegisterDataF} type="reset" value="Clean" className='w-1/2 text-center m-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer hover:scale-95 hover:shadow-none '/>
    <input onClick={()=>setLoginOrRegister('login')} type="button" value="Login" className='w-1/2 text-center m-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer hover:scale-95 hover:shadow-none '/>
 
    </div>
--- frontend/src/pages/LoginRegister.tsx
+++ frontend/src/pages/LoginRegister.tsx
@@ -17,7 +17,7 @@ const LoginRegister = () => {
     const dispatch = useDispatch()
     const selectedTheme = useSelector(selecTheme)
 
-const [loginOrRegister,setLoginOrRegister] = useState<loginOrRegisterType>('')
+const [loginOrRegister,setLoginOrRegister] = useState<loginOrRegisterType>('login')
 
   return (
    <div className=' w-full  flex items-center justify-center   text-sm  fixed z-50 top-28 left-0'>
@@ -25,8 +25,8 @@ const [loginOrRegister,setLoginOrRegister] = useState<loginOrRegisterType>('')
 <div className={selectedTheme === 'dark' ? 'width-xs p-10 rounded-lg flex justify-center items-center flex-col   text-lg bg-zinc-700 ' : 'width-xs p-10 rounded-lg flex justify-center items-center flex-col  text-lg   bg-zinc-200 '}  >
        
 
-       <button onClick={()=>setLoginOrRegister('login')} className={loginOrRegister !==''?'hidden': 'w-full px-3 py-1 mb-4 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Login</button>
-       <button onClick={()=>setLoginOrRegister('register')} className={loginOrRegister !==''?'hidden':'w-full px-3 py-1 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Register</button>
+       {/* <button onClick={()=>setLoginOrRegister('login')} className={loginOrRegister !==''?'hidden': 'w-full px-3 py-1 mb-4 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Login</button>
+       <button onClick={()=>setLoginOrRegister('register')} className={loginOrRegister !==''?'hidden':'w-full px-3 py-1 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Register</button> */}
 
 
 {
