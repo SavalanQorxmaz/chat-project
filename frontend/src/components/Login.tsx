8a4452e2966747f5962804a1adbd185f41f6b345
SavalanQorxmaz <savalan.qorxmaz@gmail.com>
2023-07-25 14:39:01 +0400

    login-register ui almost ready

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Login = () => {
+
+const Login = ({...props}) => {
+
+  const {loginOrRegister, setLoginOrRegister} = props
+
+  const [loginData, setLoginData] = useState({
+    userName:'',
+    password: ''
+  })
+
+  const loginF = ()=>{
+
+  }
+
+
+
   return (
-    <div>Login</div>
+    <form className='w-full flex flex-col items-center justify-center  select-none '>
+
+     
+      <label htmlFor="user-name" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
+        <span className='w-1/3 text-right mr-3'>User Name:</span>
+      <input className='w-2/3 outline-none p-1 bg-inherit' id='user-name' type="text" placeholder='Filankes' />
+      </label>
+     
+     <label htmlFor="password" className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer  '>
+      <span className='w-1/3 text-right mr-3'>Parol:</span>
+     <input className='w-2/3 outline-none p-1 bg-inherit' id='password' type="password" placeholder='********' />
+     </label>
+      
+     <input type="submit" value="Login" className='w-full px-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none cursor-pointer'/>
+
+     <div className='w-full p-3 py-1 m-3 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 '>
+
+      <input type="reset" value="Clean" className='w-1/2 text-center m-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer hover:scale-95 hover:shadow-none '/>
+     <input onClick={()=>setLoginOrRegister('register')} type="button" value="SignUp" className='w-1/2 text-center m-2 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400 cursor-pointer hover:scale-95 hover:shadow-none '/>
+
+     </div>
+
+    </form>
   )
 }
 
--- frontend/src/pages/LoginRegister.tsx
+++ frontend/src/pages/LoginRegister.tsx
@@ -5,8 +5,9 @@ import { setTheme, selecTheme } from '../redux/themeSlice'
 
 import Login from '../components/Login'
 import Register from '../components/Register'
+import { useState } from 'react'
 
-
+type loginOrRegisterType = ''| 'login' | 'register'
 
 
 const LoginRegister = () => {
@@ -16,16 +17,30 @@ const LoginRegister = () => {
     const dispatch = useDispatch()
     const selectedTheme = useSelector(selecTheme)
 
-
+const [loginOrRegister,setLoginOrRegister] = useState<loginOrRegisterType>('')
 
   return (
-   <div className=' w-full h-96 flex items-center justify-center   text-sm  fixed z-50 top-20 left-0'>
+   <div className=' w-full  flex items-center justify-center   text-sm  fixed z-50 top-28 left-0'>
 
 <div className={selectedTheme === 'dark' ? 'width-xs p-10 rounded-lg flex justify-center items-center flex-col   text-lg bg-zinc-700 ' : 'width-xs p-10 rounded-lg flex justify-center items-center flex-col  text-lg   bg-zinc-200 '}  >
        
 
-       <button className='w-full px-3 py-1 mb-4 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '>Login</button>
-       <button className='w-full px-3 py-1 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '>Register</button>
+       <button onClick={()=>setLoginOrRegister('login')} className={loginOrRegister !==''?'hidden': 'w-full px-3 py-1 mb-4 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Login</button>
+       <button onClick={()=>setLoginOrRegister('register')} className={loginOrRegister !==''?'hidden':'w-full px-3 py-1 rounded-lg flex items-center justify-center transition-all shadow shadow-slate-400  hover:scale-95 hover:shadow-none '}>Register</button>
+
+
+{
+  loginOrRegister==='login'?
+  <Login loginOrRegister ={loginOrRegister} setLoginOrRegister = {setLoginOrRegister}/>
+  :
+  ''
+}
+{
+  loginOrRegister==='register'?
+  <Register loginOrRegister ={loginOrRegister} setLoginOrRegister = {setLoginOrRegister}/>
+  :
+  ''
+}
 
     </div>
    </div>
